962290a1aa5727bd39d61164eb918125f6ed007c
Alberto247 <albertocarboneri99@gmail.com>
2022-05-02 20:47:48 +0200

    added base db and example API updated gitignore

--- code/server/server.js
+++ code/server/server.js
@@ -1,18 +1,25 @@
 'use strict';
 const express = require('express');
+const DAO = require('./db.js');
 // init express
 const app = new express();
 const port = 3001;
 
+const db = new DAO();
+db.createTables()
+
 app.use(express.json());
 
-//GET /api/test
-app.get('/api/hello', (req,res)=>{
-  let message = {
-    message: 'Hello World!'
-  }
-  return res.status(200).json(message);
-});
+require('./classes/InternalOrder/InternalOrderAPI.js')(app);
+require('./classes/Item/ItemAPI.js')(app);
+require('./classes/Position/PositionAPI.js')(app);
+require('./classes/RestockOrder/RestockOrderAPI.js')(app);
+require('./classes/ReturnOrder/ReturnOrderAPI.js')(app);
+require('./classes/SKU/SKUAPI.js')(app);
+require('./classes/SKUItem/SKUItemAPI.js')(app);
+require('./classes/TestDescriptor/TestDescriptorAPI.js')(app);
+require('./classes/TestResult/TestResultAPI.js')(app);
+require('./classes/User/UserAPI.js')(app);
 
 // activate the server
 app.listen(port, () => {
