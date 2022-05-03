971a7234884d2a35300051f1e29c2074cc47ac92
Alberto247 <albertocarboneri99@gmail.com>
2022-05-03 10:44:07 +0200

    finished users api refactored code

--- code/server/classes/InternalOrder/InternalOrderAPI.js
+++ code/server/classes/InternalOrder/InternalOrderAPI.js
@@ -1,31 +1,31 @@
 module.exports = function(app){
 
     app.get('/api/internalOrders', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.get('/api/internalOrdersIssued', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.get('/api/internalOrdersAccepted', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.get('/api/internalOrders/:id', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.post('/api/internalOrders', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.put('/api/internalOrders/:id', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.delete('/api/internalOrders/:id', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     
--- code/server/classes/TestResult/TestResultAPI.js
+++ code/server/classes/TestResult/TestResultAPI.js
@@ -1,23 +1,23 @@
 module.exports = function(app){
 
     app.get('/api/skuitems/:rfid/testResults', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.get('/api/skuitems/:rfid/testResults/:id', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.post('/api/skuitems/testResult', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.put('/api/skuitems/:rfid/testResult/:id', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
     app.delete('/api/skuitems/:rfid/testResult/:id', function(req, res){
-        return res.status(200);
+        return res.sendStatus(200);
     });
 
 }
