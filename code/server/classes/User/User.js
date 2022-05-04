cf36f23fb932d24dd2797c0db574a99d9c05f85a
Alberto247 <albertocarboneri99@gmail.com>
2022-05-04 11:57:25 +0200

    Moved user db to UserDAO Used User class for management

--- code/server/classes/User/User.js
+++ code/server/classes/User/User.js
@@ -1,5 +1,12 @@
 class User{
-    constructor (){}
+    constructor (data){
+        this.ID = data.ID;
+        this.NAME = data.NAME;
+        this.SURNAME = data.SURNAME;
+        this.TYPE = data.TYPE;
+        this.PASSWORD = data.PASSWORD;
+        this.EMAIL = data.EMAIL;
+    }
 }
 
 module.exports=User
