db2428d9105f67096c419e1706d0b24c41434d62
s268066 <s268066@studenti.polito.it>
2022-05-10 16:56:09 +0200

    SKU and SKUItem fixed

--- code/server/classes/SKU/SKUAPI.js
+++ code/server/classes/SKU/SKUAPI.js
@@ -4,7 +4,6 @@ const skudao=new SKUDAO();
 
 module.exports = function(app){
 
-    // FUNZIONA
     app.get('/api/skus', async function(req, res){
         if(req.session.loggedin && (req.session.user.type=="manager" || req.session.user.type=="customer" || req.session.user.type=="clerk")){
             const skus = await skudao.getSkus();
@@ -14,7 +13,6 @@ module.exports = function(app){
         }
     });
 
-    // FUNZIONA
     app.get('/api/skus/:id', async function(req, res){
         if(req.session.loggedin && req.session.user.type=="manager"){
             if(req.params.id){
@@ -30,7 +28,6 @@ module.exports = function(app){
         }
     });
 
-    // FUNZIONA
     app.post('/api/sku', async function(req, res){
         if(req.session.loggedin && req.session.user.type=="manager"){
             if(req.body.description && req.body.weight && req.body.volume && req.body.notes && req.body.price && req.body.availableQuantity){
@@ -43,9 +40,6 @@ module.exports = function(app){
         }
     });
 
-    // FUNZIONA MA SI DEVE VERIFICARE CON POSITION, POSITION SAREBBE L'ID???
-    // Modify an existing SKU. When a newAvailableQuantity is sent, occupiedWeight and occupiedVolume fields of the position 
-    // (if the SKU is associated to a position) are modified according to the new available quantity.
     app.put('/api/sku/:id', async function(req, res){
         if(req.session.loggedin && req.session.user.type=="manager"){
             if(req.params.id && req.body.newDescription && req.body.newWeight && req.body.newVolume && req.body.newNotes && req.body.newPrice && req.body.newAvailableQuantity){
@@ -69,9 +63,6 @@ module.exports = function(app){
         }
     });
 
-    // RIVEDERE MEGLIO
-    // Add or modify position of a SKU. When a SKU is associated to a position, occupiedWeight and 
-    // occupiedVolume fields of the position are modified according to the available quantity.
     app.put('/api/sku/:id/position', async function(req, res){
         if(req.session.loggedin && req.session.user.type=="manager"){
             if(req.params.id && req.body.position){
@@ -79,18 +70,22 @@ module.exports = function(app){
                 if(sku!=null){
                     const weight = sku.availableQuantity * sku.weight;
                     const volume = sku.availableQuantity * sku.volume;
-                    // controllare posizione await skudao.existing rfid
-                    if(sku.position != null){
-                        await skudao.modifySKUPosition(req.body.position, req.params.id); 
-                        await skudao.updatePositionWeightVolume(req.body.position, weight, volume);
-                        await skudao.updatePositionWeightVolume(sku.position, 0, 0);
-                        } else {
-                        await skudao.addPosition(req.body.position, req.params.id); 
-                        await skudao.updatePositionWeightVolume(req.body.position, weight, volume);
-                    } 
-                    return res.sendStatus(200);
+                    const pos = await skudao.existingPosition(req.body.position)
+                    if (pos){ 
+                        if(sku.position != null){
+                            await skudao.modifySKUPosition(req.body.position, req.params.id); 
+                            await skudao.updatePositionWeightVolume(req.body.position, weight, volume);
+                            await skudao.updatePositionWeightVolume(sku.position, 0, 0);
+                            } else {
+                            await skudao.addPosition(req.body.position, req.params.id); 
+                            await skudao.updatePositionWeightVolume(req.body.position, weight, volume);
+                        } 
+                        return res.sendStatus(200);
+                    } else {
+                        return res.sendStatus(422);    
+                    }  
                 }
-                    return res.sendStatus(404);
+                return res.sendStatus(404);
             }
             return res.sendStatus(422);
         }else{
@@ -98,14 +93,17 @@ module.exports = function(app){
         }
     });
 
-    // FUNZIONA
     app.delete('/api/skus/:id', async function(req, res){
         if(req.session.loggedin && req.session.user.type=="manager"){
             if(req.params.id){
                 const sku = await skudao.getSKUByID(req.params.id);
                 if(sku!=null){
+                    if(await skudao.existingSKUItem(req.params.id)){
+                        return res.sendStatus(422);
+                    } else {
                     await skudao.deleteSKU(req.params.id);
                     return res.sendStatus(204);
+                    }
                 }
                 return res.sendStatus(422);
             }
