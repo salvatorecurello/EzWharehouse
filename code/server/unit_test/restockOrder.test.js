62155b7811d17124040fe7b71150ba1588e7aadc
GIOVANNI RIELA s256834 <s256834@studenti.polito.it>
2022-05-21 18:15:09 +0200

    Code Refactoring

---
+++ code/server/unit_test/restockOrder.test.js
@@ -0,0 +1,10 @@
+const RestockOrderDAO = require('../RestockOrder/RestockOrderDAO.js');
+
+describe('Test RestockOrders', () => {
+	beforeAll(async () => {
+		const RoDAO = new RestockOrderDAO();
+		
+	});
+
+
+});
