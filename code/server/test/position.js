189f27a352845ddff622764a974ed37ca5eb4e41
victor3099 <vittorio.arpino@studenti.polito.it>
2022-05-24 15:51:50 +0200

    Some changes about Position Test

--- code/server/test/position.js
+++ code/server/test/position.js
@@ -6,91 +6,91 @@ const app = require("../server.js");
 var agent = chai.request.agent(app);
 
 
-describe("POST /api/position", function(){
-    it('Creates a new Position', function(done){
+describe("POST /api/position", function () {
+    it('Creates a new Position', function (done) {
         agent.post("/api/position")
-        .send({positionID: '123412341234', col: '1234', row: '1234', maxWeight: 20, maxVolume: 20, aisleID: '1234'})
-        .then(function(res){
-            res.should.have.status(201);
-            done();
-        })
-        
+            .send({ positionID: '123412341234', col: '1234', row: '1234', maxWeight: 20, maxVolume: 20, aisleID: '1234' })
+            .then(function (res) {
+                res.should.have.status(201);
+                done();
+            })
+
+    })
+    it('Creates a new Position with different PositionID', function (done) {
+        agent.post("/api/position")
+            .send({ positionID: '1234213212', col: '1234', row: '1234', maxWeight: 20, maxVolume: 20, aisleID: '1234' })
+            .then(function (res) {
+                res.should.have.status(422);
+                done();
+            })
     })
 });
 
-describe("GET /api/positions", function(){
-    it('Return an array containing all positions', function(done){
+describe("GET /api/positions", function () {
+    it('Return an array containing all positions', function (done) {
         agent.get("/api/positions")
-        .then(function(res){
-            res.should.have.status(200);
-            res.body.length.should.be.at.least(1);
-            done();
-        })
+            .then(function (res) {
+                res.should.have.status(200);
+                res.body.length.should.be.at.least(1);
+                done();
+            })
     })
 });
 
-describe("PUT /api/position", function(){
-    it('Modify a position identified by positionID: 123412341234', function(done){
+describe("PUT /api/position", function () {
+    it('Modify a position identified by positionID: 123412341234', function (done) {
         agent.put("/api/position/123412341234")
-        .send({newAisleID: '8002', newRow: '3454', newCol: '3412', newMaxWeight: 1200, newMaxVolume: 600,newOccupiedWeight: 200, newOccupiedVolume: 100})
-        .then(function(res){
-            res.should.have.status(200);
-            // agent.get("/api/users")
-            // .then(function(res){
-            //     res.should.have.status(200);
-            //     emails=[]
-            //     res.body.map((x)=>{emails.push(x.email)})
-            //     emails.should.contain("lucaardito@ezwh.it")
-            //     for(let user of res.body){
-            //         if(user.email==="lucaardito@ezwh.it"){
-            //             user.type.should.equal("supplier");
-            //         }
-                // }
+            .send({ newAisleID: '8002', newRow: '3454', newCol: '3412', newMaxWeight: 1200, newMaxVolume: 600, newOccupiedWeight: 200, newOccupiedVolume: 100 })
+            .then(function (res) {
+                res.should.have.status(200);
                 done();
             })
-        })
     })
-// })
+    it('Modify a position with error 404 not found position', function (done) {
+        agent.put("/api/position/123412341234")
+            .send({ newAisleID: '8002', newCol: '3412', newRow: '3454', newMaxWeight: 1200, newMaxVolume: 600, newOccupiedWeight: 200, newOccupiedVolume: 100 })
+            .then(function (res) {
+                res.should.have.status(404);
+                done();
+            })
+    })
+})
 
-describe("PUT /api/position/changeID", function(){
-    it('Modify the positionID of a position, given its old positionID: 123412341234', function(done){
+describe("PUT /api/position/changeID", function () {
+    it('Modify the positionID of a position, given its old positionID: 123412341234', function (done) {
         agent.put("/api/position/800234543412/changeID")
-        .send({newPositionID: '123412341234'})
-        .then(function(res){
-            res.should.have.status(200);
-            // agent.get("/api/users")
-            // .then(function(res){
-            //     res.should.have.status(200);
-            //     emails=[]
-            //     res.body.map((x)=>{emails.push(x.email)})
-            //     emails.should.contain("lucaardito@ezwh.it")
-            //     for(let user of res.body){
-            //         if(user.email==="lucaardito@ezwh.it"){
-            //             user.type.should.equal("supplier");
-            //         }
-            //     }
+            .send({ newPositionID: '123412341234' })
+            .then(function (res) {
+                res.should.have.status(200);
                 done();
             })
-        })
     })
-// })
+    it('Modify the positionID of a position, given its old positionID: 123412341234', function (done) {
+        agent.put("/api/position/ededwdwwss/changeID")
+            .send({ newPositionID: '123412341234' })
+            .then(function (res) {
+                res.should.have.status(404);
+                done();
+            })
+    })
+});
 
-describe("DELETE /api/position", function(){
-    it('Delete a position receiving its positionID: 123412341234', function(done){
+describe("DELETE /api/position", function () {
+    it('Delete a position receiving its positionID: 123412341234', function (done) {
         agent.delete("/api/position/123412341234")
-        .then(function(res){
-            res.should.have.status(204);
-            // agent.get("/api/users")
-            // .then(function(res){
-            //     res.should.have.status(200);
-            //     for(let user of res.body){
-            //         if(user.email=="lucaardito@ezwh.it"){
-            //             user.type.should.not.equal("supplier");
-            //         }
-                // }
+            .then(function (res) {
+                res.should.have.status(204);
                 done();
             })
-        })
     })
-// })
+    it('Delete a position receiving a position not found', function (done) {
+        agent.delete("/api/position/sadaddasad")
+            .then(function (res) {
+                res.should.have.status(422);
+                done();
+            })
+    })
+
+
+})
 
