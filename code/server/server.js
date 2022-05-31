8aab2d0a53be186fbaa1b14ea806d2ee10041d8a
s268066 <s268066@studenti.polito.it>
2022-05-31 20:29:33 +0200

    moved user creation after app start

--- code/server/server.js
+++ code/server/server.js
@@ -29,13 +29,12 @@ require('./classes/User/UserAPI.js')(app);
 
 
 Promise.all(db.createTables()).then(() => {
-  Promise.all(db.createDefaultUsers()).then(() => {
+ // Promise.all(db.createDefaultUsers()).then(() => {
     app.listen(port, () => {
       console.log(`Server listening at http://localhost:${port}`);
     });
-  })
+  //})
 })
-
-
+db.createDefaultUsers()
 
 module.exports = app;
